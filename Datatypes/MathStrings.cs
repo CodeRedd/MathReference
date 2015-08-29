244315fc8450d71d9a857a932a7797a65addaea1
Culver Redd <culver@code-redd.com>
2015-08-29 17:34:17 -0400

    Add constants for common unicode math symbols

---
+++ Datatypes/MathStrings.cs
@@ -0,0 +1,48 @@
+﻿namespace MathReference.Datatypes
+{
+   class MathStrings
+   {
+      #region Accents
+      public const string PRIME           = "\u0301";
+      public const string HAT             = "\u0302";
+      public const string DOT             = "\u0307";
+      public const string DOUBLEDOT       = "\u0308";
+      public const string VECTOR          = "\u20D7";
+      #endregion                             
+                                             
+      #region Operators                      
+      public const string SQRT            = "\u221A";
+      public const string DOTPRODUCT      = "\u00B7";
+      public const string CROSSPRODUCT    = "\u00D7";
+      public const string INTEGRAL        = "\u222B";
+      public const string CLOSEDINTEGRAL  = "\u222E";
+      public const string SUM             = "\u2211";
+      public const string DEL             = "\u2207";
+      public const string PARTIALDIFF     = "\u2202";
+      #endregion                             
+                                             
+      #region Subscript and Superscript      
+      public const string SQUARED         = "\u00B2";
+      public const string CUBED           = "\u00B3";
+      public const string NAUGHT          = "\u2080";
+      public const string SUBONE          = "\u2081";
+      public const string SUBTWO          = "\u2082";
+      public const string SUBN            = "\u2099";
+      #endregion                             
+                                             
+      #region Greek                          
+      public const string DELTA           = "\u03B4";
+      public const string EPSILON         = "\u03B5";
+      public const string MU              = "\u03BC";
+      public const string PI              = "\u03C0";
+      public const string PHI             = "\u03C6";
+      public const string OMEGA           = "\u03C9";
+      public const string RHO             = "\u03C1";
+      public const string SIGMA           = "\u03C3";
+      public const string TAU             = "\u03C4";
+      public const string THETA           = "\u03B8";
+      public const string PLANCK_CONST    = "\u210E";
+      public const string CAPITALPHI      = "\u03D5";
+      #endregion
+   }
+}
