30cb26b1b643a26665febc0fc31bbad55cfac797
Culver Redd <culver@code-redd.com>
2015-09-15 21:52:25 -0400

    Add base model for individual equation

---
+++ Model/Equation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+
+namespace MathReference.Model
+{
+   class Equation
+   {
+      private List<string> ParentCategories { get; set; }
+
+      public Equation( ObservableCollection<Symbol> equationSymbols, List<string> categories )
+      {
+         DisplaySymbols = equationSymbols;
+         ParentCategories = categories;
+      }
+
+      #region Properties
+
+      public string Title { get; set; }
+      public ObservableCollection<Symbol> DisplaySymbols { get; set; }
+
+      #endregion
+
+      public bool HasTag( string tag )
+      {
+         return ParentCategories.Contains( tag );
+      }
+   }
+}
