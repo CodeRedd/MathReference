81d11ebaf83d19b10caef2c0ddc71e7b73d4a729
Culver Redd <culver@code-redd.com>
2015-09-15 22:05:13 -0400

    Flesh out viewmodel with collections and initters

--- ViewModel/EquationManager.cs
+++ ViewModel/EquationManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using MathReference.Model;
 
 namespace MathReference.ViewModel
 {
@@ -7,11 +8,79 @@ namespace MathReference.ViewModel
    {
       public EquationManager()
       {
-         EquationList = new ObservableCollection<string>() { "ListItem1", "ListItem2", "ListItem3" };
+         EquationList = new ObservableCollection<Equation>();
+         CategoryList = new ObservableCollection<string>();
+
+         addEquationCategories();
+
+         addCylindricalCoordinateConversions();
+         addSphericalCoordinateConversions();
+         addTrigIdentities();
+         addLinearAlgebra();
+         addCalculusLaws();
+         addMathTools();
+         addSeriesExpansions();
+         addQuantumBasics();
       }
 
-      #region Properties
-      public ObservableCollection<string> EquationList { get; set; }
+      public ObservableCollection<Equation> EquationList { get; set; }
+      public ObservableCollection<string> CategoryList { get; set; }
+
+      #region Initializers
+      //populates combo box full of categories
+      private void addEquationCategories()
+      {
+
+      }
+
+      //conversion between cartesian and cylindrical
+      private void addCylindricalCoordinateConversions()
+      {
+
+      }
+
+      //conversion between cartesian and spherical
+      private void addSphericalCoordinateConversions()
+      {
+
+      }
+
+      //trigonometric identities
+      private void addTrigIdentities()
+      {
+
+      }
+
+      //matrix math tools (normalization, determinant, etc)
+      private void addLinearAlgebra()
+      {
+
+      }
+
+      //calculus-related laws (divergence, curl, divergence theorem, green's theorem, etc)
+      private void addCalculusLaws()
+      {
+
+      }
+
+      //Mathematical tools (fourier transform, gamma functions, beta functions, etc)
+      private void addMathTools()
+      {
+
+      }
+
+      //Polynomial expansions (legendre polynomials, hermitian polynomials, etc)
+      private void addSeriesExpansions()
+      {
+
+      }
+
+      //Basics of quantum like waveforms and hamiltonians
+      private void addQuantumBasics()
+      {
+
+      }
+
       #endregion
    }
 }
