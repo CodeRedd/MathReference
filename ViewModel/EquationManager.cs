fa7be957cf57b211960a9856c11dae3b02f88365
Culver Redd <culver@code-redd.com>
2015-09-20 16:27:43 -0400

    Populate combo box and autoselect first entry

--- ViewModel/EquationManager.cs
+++ ViewModel/EquationManager.cs
@@ -10,7 +10,6 @@ namespace MathReference.ViewModel
       {
          EquationList = new ObservableCollection<Equation>();
          CategoryList = new ObservableCollection<string>();
-
          addEquationCategories();
 
          addCylindricalCoordinateConversions();
@@ -25,12 +24,21 @@ namespace MathReference.ViewModel
 
       public ObservableCollection<Equation> EquationList { get; set; }
       public ObservableCollection<string> CategoryList { get; set; }
+      public int SelectedCategory { get; set; }
 
       #region Initializers
       //populates combo box full of categories
       private void addEquationCategories()
       {
-
+         CategoryList.Add( "Cylindrical Coordinates" );
+         CategoryList.Add( "Spherical Coordinates" );
+         CategoryList.Add( "Trigonometric Identities" );
+         CategoryList.Add( "Linear Algebra" );
+         CategoryList.Add( "Calculus Laws" );
+         CategoryList.Add( "Mathematical Tools" );
+         CategoryList.Add( "Series Expansions" );
+         CategoryList.Add( "Quantum Basics" );
+         SelectedCategory = 0;
       }
 
       //conversion between cartesian and cylindrical
